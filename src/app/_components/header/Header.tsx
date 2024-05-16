bbb974deff20d100818d41af10295b2ab26c6418
amanj <a.ghaderi@ria.co.ir>
2024-05-17 00:31:15 +0330

    init

---
+++ src/app/_components/header/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+	return <div className="flex justify-center">header</div>;
+};
+
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
 import type { Config } from "tailwindcss";
 
+export const tailwindColors: { [key: string]: string } = {
+	current: "currentColor",
+	transparent: "transparent",
+	white: "#F9F9F9",
+	primary: "#00ADB5",
+	secondary: "#343840",
+	"secondary-content": "#FFFFFF",
+	base: "#222831",
+};
+
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+
+			colors: tailwindColors,
+		},
+	},
+	plugins: [],
 };
 export default config;
