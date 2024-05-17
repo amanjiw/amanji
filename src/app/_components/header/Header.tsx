f9765186ba5aafecfc59a41b449279f9531d1e2d
amanj <ghaderi.amanj@gmail.com>
2024-05-17 10:23:47 +0330

    header

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
 	return (
-		<div className="flex items-center h-20 px-24 capitalize fixed w-full">
+		<div className="flex items-center h-20 px-24 capitalize fixed w-full ">
 			<ul className="flex gap-14 text-lg w-full">
 				<li className="flex-1">
 					<span className="cursor-pointer text-2xl font-bold ">
